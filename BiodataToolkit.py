5214453a2f4ef39388092f572433b00ffab52074
Ruben Oswaldo Duarte Bernal <ruben9_24@hotmail.com>
2019-10-15 18:54:38 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   BiodataToolkit.py
    	new file:   Main.py
    	new file:   Media/logo.ico
    	new file:   logo.ico

---
+++ BiodataToolkit.py
@@ -0,0 +1,123 @@
+#We import from biopython library the SeqIO tool
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+import time
+
+#Var definitions
+globalcount=0    
+cdsList=[]
+fullFastas={'Complete Fasta':[]}
+renamedFastas={'Renamed Fastas':[]}
+init = False
+
+start = time.time()
+
+#We define the class to define the data estructure that we are going to use.
+class DataSet:
+    
+    #Constructor Method    
+    def __init__(self,keys):
+        self.data={}
+        for i in keys:
+            self.data[i]=[]
+        self.initialized = True
+
+class Cdstype:
+    
+    #Contructor method
+    def __init__(self,tipo,keys):
+        self.type = str(tipo)
+        self.data = {}
+        for i in keys:
+            self.data[i]=[]
+        self.data['CDS']=[]
+        self.data['Fasta'] = []
+
+        
+def createNewName(country,sequence,segment,counter):
+    newName=">"
+    countries={"Afghanistan":["A","AF"],"Aland Islands":["E","AX"],"Albania":["E","AL"],"Algeria":["F","DZ"],"American Samoa":["O","AS"],"Andorra":["E","AD"],"Angola":["F","AO"],"Anguilla":["L","AI"],"Antarctica":["S","AQ"],"Antigua and Barbuda":["L","AG"],"Argentina":["L","AR"],"Armenia":["A","AM"],"Aruba":["L","AW"],"Australia":["O","AU"],"Austria":["E","AT"],"Azerbaijan":["A","AZ"],"Bahamas":["L","BS"],"Bahrain":["A","BH"],"Bangladesh":["A","BD"],"Barbados":["L","BB"],"Belarus":["E","BY"],"Belgium":["E","BE"],"Belize":["L","BZ"],"Benin":["F","BJ"],"Bermuda":["L","BM"],"Bhutan":["A","BT"],"Bolivia":["L","BO"],"Bosnia and Herzegovina":["E","BA"],"Botswana":["F","BW"],"Bouvet Island":["E","BV"],"Brazil":["L","BR"],"British Indian Ocean Territory":["A","IO"],"Brunei Darussalam":["A","BN"],"Bulgaria":["E","BG"],"Burkina Faso":["F","BF"],"Burundi":["F","BI"],"Cambodia":["A","KH"],"Cameroon":["F","CM"],"Canada":["N","CA"],"Cape Verde":["F","CV"],"Cayman Islands":["L","KY"],"Central African Republic":["F","CF"],"Chad":["F","TD"],"Chile":["L","CL"],"China":["A","CN"],"Christmas Island":["O","CX"],"Cocos (Keeling) Islands":["A","CC"],"Colombia":["L","CO"],"Comoros":["F","KM"],"Congo":["F","CG"],"Congo":["F","CD"],"Cook Islands":["O","CK"],"Costa Rica":["L","CR"],"Cote d'Ivoire":["F","CI"],"Croatia":["E","HR"],"Cuba":["L","CU"],"Cyprus":["E","CY"],"Czech Republic":["E","CZ"],"Denmark":["E","DK"],"Djibouti":["F","DJ"],"Dominica":["L","DM"],"Dominican Republic":["L","DO"],"Ecuador":["L","EC"],"Egypt":["F","EG"],"El Salvador":["L","SV"],"Equatorial Guinea":["F","GQ"],"Eritrea":["F","ER"],"Estonia":["E","EE"],"Ethiopia":["F","ET"],"Falkland Islands (Malvinas)":["L","FK"],"Faroe Islands":["E","FO"],"Fiji":["O","FJ"],"Finland":["E","FI"],"France":["E","FR"],"French Guiana":["L","GF"],"French Polynesia":["O","PF"],"French Southern Territories":["E","TF"],"Gabon":["F","GA"],"Gambia":["F","GM"],"Georgia":["E","GE"],"Germany":["E","DE"],"Ghana":["F","GH"],"Gibraltar":["E","GI"],"Greece":["E","GR"],"Greenland":["L","GL"],"Grenada":["L","GD"],"Guadeloupe":["L","GP"],"Guam":["O","GU"],"Guatemala":["L","GT"],"Guernsey":["E","GG"],"Guinea":["F","GN"],"Guinea-Bissau":["F","GW"],"Guyana":["L","GY"],"Haiti":["L","HT"],"Heard Island and McDonald Islands":["O","HM"],"Holy See (Vatican City State)":["E","VA"],"Honduras":["L","HN"],"Hong Kong":["A","HK"],"Hungary":["E","HU"],"Iceland":["E","IS"],"India":["A","IN"],"Indonesia":["A","ID"],"Iran":["A","IR"],"Iraq":["A","IQ"],"Ireland":["E","IE"],"Isle of Man":["E","IM"],"Israel":["A","IL"],"Italy":["E","IT"],"Jamaica":["L","JM"],"Japan":["A","JP"],"Jersey":["E","JE"],"Jordan":["A","JO"],"Kazakhstan":["A","KZ"],"Kenya":["F","KE"],"Kiribati":["O","KI"],"Korea":["A","KP"],"South Korea":["A","KR"],"Kuwait":["A","KW"],"Kyrgyzstan":["A","KG"],"Lao People's Democratic Republic":["A","LA"],"Latvia":["E","LV"],"Lebanon":["A","LB"],"Lesotho":["F","LS"],"Liberia":["F","LR"],"Libyan Arab Jamahiriya":["F","LY"],"Liechtenstein":["E","LI"],"Lithuania":["E","LT"],"Luxembourg":["E","LU"],"Macao":["A","MO"],"Macedonia, the former Yugoslav Republic of":["E","MK"],"Madagascar":["F","MG"],"Malawi":["F","MW"],"Malaysia":["A","MY"],"Maldives":["A","MV"],"Mali":["F","ML"],"Malta":["E","MT"],"Marshall Islands":["O","MH"],"Martinique":["L","MQ"],"Mauritania":["F","MR"],"Mauritius":["F","MU"],"Mayotte":["F","YT"],"Mexico":["N","MX"],"Micronesia, Federated States of":["O","FM"],"Moldova, Republic of":["E","MD"],"Monaco":["E","MC"],"Mongolia":["A","MN"],"Montenegro":["E","ME"],"Montserrat":["L","MS"],"Morocco":["F","MA"],"Mozambique":["F","MZ"],"Myanmar":["A","MM"],"Namibia":["F","NA"],"Nauru":["O","NR"],"Nepal":["A","NP"],"Netherlands":["E","NL"],"Netherlands Antilles":["E","AN"],"New Caledonia":["O","NC"],"New Zealand":["O","NZ"],"Nicaragua":["L","NI"],"Niger":["F","NE"],"Nigeria":["F","NG"],"Niue":["O","NU"],"Norfolk Island":["O","NF"],"Northern Mariana Islands":["O","MP"],"Norway":["E","NO"],"Oman":["A","OM"],"Pakistan":["A","PK"],"Palau":["O","PW"],"Palestinian Territory, Occupied":["A","PS"],"Panama":["L","PA"],"Papua New Guinea":["O","PG"],"Paraguay":["L","PY"],"Peru":["L","PE"],"Philippines":["A","PH"],"Pitcairn":["O","PN"],"Poland":["E","PL"],"Portugal":["E","PT"],"Puerto Rico":["L","PR"],"Qatar":["A","QA"],"Reunion  Réunion":["F","RE"],"Romania":["E","RO"],"Russia":["A","RU"],"Rwanda":["F","RW"],"Saint Barthélemy":["L","BL"],"Saint Helena":["F","SH"],"Saint Kitts and Nevis":["L","KN"],"Saint Lucia":["L","LC"],"Saint Martin (French part)":["L","MF"],"Saint Pierre and Miquelon":["L","PM"],"Saint Vincent and the Grenadines":["L","VC"],"Samoa":["O","WS"],"San Marino":["E","SM"],"Sao Tome and Principe":["F","ST"],"Saudi Arabia":["A","SA"],"Senegal":["F","SN"],"Serbia":["E","RS"],"Seychelles":["F","SC"],"Sierra Leone":["F","SL"],"Singapore":["A","SG"],"Slovakia":["E","SK"],"Slovenia":["E","SI"],"Solomon Islands":["O","SB"],"Somalia":["F","SO"],"South Africa":["F","ZA"],"South Georgia and the South Sandwich Islands":["E","GS"],"Spain":["E","ES"],"Sri Lanka":["A","LK"],"Sudan":["F","SD"],"Suriname":["L","SR"],"Svalbard and Jan Mayen":["E","SJ"],"Swaziland":["F","SZ"],"Sweden":["E","SE"],"Switzerland":["E","CH"],"Syrian Arab Republic":["A","SY"],"Taiwan":["A","TW"],"Tajikistan":["A","TJ"],"Tanzania":["F","TZ"],"Thailand":["A","TH"],"Timor-Leste":["A","TL"],"Togo":["F","TG"],"Tokelau":["O","TK"],"Tonga":["O","TO"],"Trinidad and Tobago":["L","TT"],"Tunisia":["F","TN"],"Turkey":["A","TR"],"Turkmenistan":["A","TM"],"Turks and Caicos Islands":["L","TC"],"Tuvalu":["O","TV"],"Uganda":["F","UG"],"Ukraine":["E","UA"],"United Arab Emirates":["A","AE"],"United Kingdom":["E","GB"],"United States":["N","US"],"United States Minor Outlying Islands":["L","UM"],"Uruguay":["L","UY"],"USA":["N","US"],"Uzbekistan":["A","UZ"],"Vanuatu":["O","VU"],"Venezuela":["L","VE"],"Viet Nam":["A","VN"],"Virgin Islands, British":["L","VG"],"Virgin Islands, U.S.":["L","VI"],"Wallis and Futuna":["O","WF"],"Western Sahara":["F","EH"],"Yemen":["A","YE"],"Zambia":["F","ZM"],"Zimbabwe":["F","ZW"]}
+    states={"Alabama":"AL","Alaska":"AK","Arizona":"AZ","Arkansas":"AR","California":"CA","Colorado":"CO","Connecticut":"CT","Delaware":"DE","District of Columbia":"DC","Florida":"FL","Georgia":"GA","Hawaii":"HI","Idaho":"ID","Illinois":"IL","Indiana":"IN","Iowa":"IA","Kansas":"KS","Kentucky":"KY","Louisiana":"LA","Maine":"ME","Maryland":"MD","Massachusetts":"MA","Michigan":"MI","Minnesota":"MN","Mississippi":"MS","Missouri":"MO","Montana":"MT","Nebraska":"NE","Nevada":"NV","New Hampshire":"NH","New Jersey":"NJ","New Mexico":"NM","New York":"NY","North Carolina":"NC","North Dakota":"ND","Ohio":"OH","Oklahoma":"OK","Oregon":"OR","Pennsylvania":"PA","RhodeIsland":"RI","South Carolina":"SC","South Dakota":"SD","Tennessee":"TN","Texas":"TX","Utah":"UT","Vermont":"VT","Virginia":"VA","Washington":"WA","West Virginia":"WV","Wisconsin":"WI","Wyoming":"WY"}
+    
+    if ':'in country:
+        country=country[0:country.index(':')]
+    if country in countries.keys():
+        newName=newName+str(segment)+countries[country][0] + countries[country][1]
+        if country == "United States"or "USA":
+            for i in states.keys():
+                if i in sequence:
+                    newName=newName + states[i] 
+    else:
+        for y in countries.keys():
+            if y in sequence:
+                newName=newName+str(segment)+countries[y][0] + countries[y][1]
+                break
+            else:
+                newName=newName+str(segment)+'XX'
+                break
+    newName=newName+str(counter)
+    return newName
+
+
+def parser(filename,fileformat):
+    #We read the sequence genbank file
+    for seq_record in SeqIO.parse("segment4_2017_all.gb", "genbank"):
+        
+        fullFastas['Complete Fasta'].append(seq_record.format('fasta'))
+        
+        #first we are going to initialize our data structures
+        if init != True:
+            #We pull out the keys that we're going to use for the data structure
+            for feature in seq_record.features:
+                #Verify the type of feature
+                if feature.type == 'source':
+                    #Create the main Dataset usign the DataSet class
+                    dataSet=DataSet(feature.qualifiers.keys())
+                #Verify the type of the feature for Codon Sequences
+                if feature.type == "CDS":
+                    try:
+                        cds=Cdstype(feature.qualifiers['gene'][0],feature.qualifiers.keys())
+                        cdsList.append(cds)
+                    except:
+                        cds=Cdstype(feature.qualifiers['product'][0],feature.qualifiers.keys())
+                        cdsList.append(cds)
+            #Initialization Validation
+            init = True
+        #Collect all the features selected in the first file as the set of interest
+        for feature in seq_record.features:
+            #First we go for the Codon Sequence feature in the file.
+            if feature.type == "CDS":
+                for item in cdsList:
+                    try:
+                        if item.type == feature.qualifiers['gene'][0]:
+                            for y in feature.qualifiers.keys():
+                                if y in item.data.keys():
+                                    item.data[y].append(feature.qualifiers[y][0])
+                            #We save the location of the codon Sequence        
+                            item.data['CDS'].append(str(feature.location))
+                            #We pull out the sequence as string to join the string with the new
+                            myseq2 = str(feature.location.extract(seq_record.seq))
+                            #We pull out the sequence in fasta format to keep the original name
+                            myseq=feature.location.extract(seq_record).format('fasta')
+                            #We save the original fasta format sequence
+                            item.data['Fasta'].append(myseq)
+                            #We trimm the sequence for the creation of the new name
+                            myseq=myseq[0:myseq.index('\n')]
+                    except:
+                        try:
+                            item.data['CDS'].append('empty')
+                            item.data['Fasta'].append('empty')
+                        except:
+                            print('Fatal Error')
+                            continue
+            if feature.type =='source':
+                            for y in dataSet.data.keys():
+                                if y in feature.qualifiers.keys():
+                                    dataSet.data[y].append(feature.qualifiers[y][0])
+                                else:
+                                    dataSet.data[y].append('empty')
+
+                            
+
+
+    
---
+++ Main.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import tkinter.messagebox
+
+root=tk.Tk()
+#Here it goes the window title
+root.title('BiodataToolkit')
+#Here it goes the window size
+width=600
+height=480
+#Centering the window
+screen_width=root.winfo_screenwidth()
+screen_height=root.winfo_screenheight()
+x_coordinate= (screen_width/2) - (width/2)
+y_coordinate= (screen_height/2) - (height/2)
+root.geometry('%dx%d+%d+%d' % (width,height,x_coordinate,y_coordinate))
+#Fixing size
+root.resizable(0,0)
+
+#This was supposed to insert an icon in the windows title
+#root.iconbitmap(r'Media/logo2.ico')
+
+#logo = tk.PhotoImage(file='/Users/rduarte/Documents/Projects/GCL/Biodata Toolkit GUI/logo.ico')
+#root.tk.call('wm', 'iconphoto', root._w, logo)
+
+
+#Code to add widgets goes HERE !
+menuBar=tk.Menu(root)
+root.config(menu=menuBar)
+
+#Functions asociated to the next menu section
+def test_function():
+    print('this is the test')
+# create a pulldown menu, and add it to the menu bar
+filemenu = tk.Menu(menuBar, tearoff=0)
+filemenu.add_command(label="Open", command=test_function())
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menuBar.add_cascade(label="File", menu=filemenu)
+
+#Functions asociated to the next menu section
+# Menu Definition
+editmenu = tk.Menu(menuBar, tearoff=0)
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+menuBar.add_cascade(label="Edit", menu=editmenu)
+
+#Functions asociated to the next menu section
+def about_us():
+    tkinter.messagebox.showinfo('BiodataToolkit','Laboratorio de Genómica de Celomados - BivL2ab')
+#Menu definition
+helpmenu = tk.Menu(menuBar, tearoff=0)
+helpmenu.add_command(label="About", command=about_us)
+menuBar.add_cascade(label="Help", menu=helpmenu)
+
+
+
+
+
+
+root.mainloop()
+
